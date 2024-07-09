4470b0dca15191f6fbfa64e4c316e2116b28cfef
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-07-09 01:31:18 -0500

    Initial Test

---
+++ 1brc.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+struct cityStruct {
+    float total = 0;
+    int steps = 0;
+
+    float min = 999;
+    float max = -999;
+
+    string name;
+    bool isEmpty() const {return name == "";}
+};
+
+void addData(map <string,cityStruct> cities, string nameCity, string tempCity) {
+    float tmpFCity;
+    tmpFCity = stof(tempCity);
+
+    cityStruct city = cities[nameCity];
+
+    city.total = city.total + tmpFCity;
+    city.steps = city.steps + 1;
+
+    if (city.min > tmpFCity) {
+        city.min = tmpFCity;
+    }
+
+    if (city.max < tmpFCity) {
+        city.max = tmpFCity;
+    }
+
+}
+
+int main() {
+    long long steps = 0;
+    map <string, cityStruct> cities;
+    ifstream inputFile;
+    ofstream outputFile;
+
+    inputFile.open("measurements.txt", ios::in);
+    string line;
+    string nameCity, tempCity;
+
+    while(getline(inputFile, line))
+    {
+        if(steps % 10000 == 0) {
+            cout << steps << endl;
+        }
+        stringstream stream(line);
+
+        getline(stream, nameCity, ';');
+        getline(stream, tempCity, ';');
+
+        addData(cities, nameCity, tempCity);
+
+        steps = steps + 1;
+    }
+
+    string cityString;
+    outputFile.open("./output.txt");
+    for ( const auto & city : cities) {
+        cityString = city.first + ";" + to_string(city.second.min) + ";" + to_string(city.second.total/city.second.steps) + ";" + to_string(city.second.max);
+        outputFile << cityString;
+    }
+    outputFile.close();
+}
